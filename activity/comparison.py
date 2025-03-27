58c482e4a7524feb0954a4ffe1cd885a0e39b845
Gitika Kalal <gitikakalal21@gmail.com>
2025-03-27 11:15:08 -0700

    completed activity1

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,7 @@
 # add your get_student_with_more_classes function here!
+
+def get_student_with_more_classes(student1, student2):
+    if student1.get_num_classes() > student2.get_num_classes():
+        print(student1.name)
+    else:
+        print(student2.name)
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,16 @@
 # add your Student class here!
+class Student:
+    def __init__(self, name, year, courses):
+        self.name = name
+        self.year = year
+        self.courses = courses
+
+    def add_class(self, course):
+        self.courses.append(course)
+        print(self.courses)
+
+    def get_num_classes(self):
+        return len(self.courses)
+    
+    def summary(self):
+        print(f"{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes")
